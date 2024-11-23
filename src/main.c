6261a2e8a646dccdba6642e975b4ff7a988292e4
Simon Nawara <sinawara@shi-r5-p3.s19.be>
2024-11-23 15:05:36 +0100

    added basics again

--- src/main.c
+++ src/main.c
@@ -1,28 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sinawara <sinawara@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/23 13:25:52 by sinawara          #+#    #+#             */
-/*   Updated: 2024/11/23 13:26:47 by sinawara         ###   ########.fr       */
+/*   Created: 2024/11/23 13:24:36 by sinawara          #+#    #+#             */
+/*   Updated: 2024/11/23 13:25:50 by sinawara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "../includes/pipex.h"
 
-
-# include "../libft/include/ft_printf.h"
-# include "../libft/include/get_next_line.h"
-# include "../libft/include/libft.h"
-# include <fcntl.h>
-# include <limits.h>
-# include <math.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-
-#endif
+int main(void)
+{
+	ft_printf("Makefile Works!\n");
+	return (0);
+}
