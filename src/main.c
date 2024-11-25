c2fdf08c718e0517bc2461efd481d440b5a5a371
Simon Nawara <sinawara@shi-r5-p3.s19.be>
2024-11-25 12:00:43 +0100

    tests

--- src/main.c
+++ src/main.c
@@ -6,14 +6,26 @@
 /*   By: sinawara <sinawara@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 13:24:36 by sinawara          #+#    #+#             */
-/*   Updated: 2024/11/23 13:25:50 by sinawara         ###   ########.fr       */
+/*   Updated: 2024/11/25 12:00:02 by sinawara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-int main(void)
+int main(int argc, char **argv, char **env)
 {
-	ft_printf("Makefile Works!\n");
+
+	/*
+	int i = 0;
+	while (env[i])
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
+	*/
+
+	int fd = open("./t.txt", O_WRONLY | O_APPEND);
+	dup2(fd, 2);
+	perror("I will be printed in the file t.txt\n");
 	return (0);
 }
