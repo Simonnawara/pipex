37e27b4a948a052ec1e543971920d601fbf97f52
Simon Nawara <sinawara@shi-r6-p4.s19.be>
2024-11-26 16:09:48 +0100

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sinawara <sinawara@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 13:24:36 by sinawara          #+#    #+#             */
-/*   Updated: 2024/11/26 13:20:51 by sinawara         ###   ########.fr       */
+/*   Updated: 2024/11/26 16:09:09 by sinawara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,19 @@ int file_error(void)
 	exit (1);
 }
 
+char **split_path(char *path)
+{
+	return (ft_split(path, ':'));
+}
+
 int main(int argc, char **argv, char **env)
 {
-	(void) env;
+	int i = 0;
+	while (env[i])
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
 	if (argc != 5)
 	{
 		ft_printf("Error -> usage : [ ./pipex file1 cmd1 cmd2 file2 ]\n");
@@ -41,7 +51,7 @@ int main(int argc, char **argv, char **env)
 		if ((open(argv[4], O_WRONLY | O_TRUNC | O_CREAT | O_APPEND, 0777 )) < 0)
 			file_error();
 		// check if infile exists ✔
-		// create an outfile if it doesn't exist
+		// create an outfile if it doesn't exist ✔
 
 		// We will have to fork twice, and assign child1 to execute cmd1, and child2 to execute cmd2.
 		// The parent will waut at the end.
@@ -50,4 +60,57 @@ int main(int argc, char **argv, char **env)
 		
 	}
 	return (0);
+}
+// Finds the PATH variable in the environment
+char *get_path(char **env)
+{
+	int i;
+
+	i = 0;
+	while(env[i])
+	{
+		if(ft_strncmp(env[i], "PATH", 4) == 0)
+			return (env[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+char *build_path(char *cmd, char **env)
+{
+	int	i;
+	char **paths;
+	char *path_var;
+	char *full_path;
+
+	path_var = get_path(env);
+
+	if (!path_var)
+		return (NULL);
+
+	paths = ft_split(path_var, ':');
+	if (!paths)
+		return (NULL);
+
+	i = 0;
+	while(paths[i])
+	{
+		full_path = ft_strjoin(paths, "/");
+		if (!full_path)
+        {
+            free_array(paths);
+            return (NULL);
+        }
+		full_path = ft_strjoin(full_path, cmd);
+		if (!full_path)
+        {
+            free_array(paths); // Free all allocated paths
+            return (NULL);
+        }
+		if (access(full_path, F_OK | X_OK) == 0);
+			return (full_path);
+		free(full_path);
+		i++;
+	}
+	return (NULL);
 }
