8baa99ec4cab2462ba9d2ca9d93a468515e2f48b
Simon Nawara <sinawara@shi-r6-p6.s19.be>
2024-11-27 11:14:15 +0100

    finalized

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sinawara <sinawara@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 13:24:36 by sinawara          #+#    #+#             */
-/*   Updated: 2024/11/26 20:10:38 by sinawara         ###   ########.fr       */
+/*   Updated: 2024/11/27 11:11:38 by sinawara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,9 @@ void *free_and_return(char **array, void *return_value)
 
 int main(int argc, char **argv, char **env)
 {
+	int infile;
+	int outfile;
+	int pipe_fd[2];
 	char *path_cmd1;
 	char *path_cmd2;
 	char **cmd1_args;
@@ -67,14 +70,16 @@ int main(int argc, char **argv, char **env)
 	{
 		if(open(argv[1], O_RDONLY) < 0)		// check if infile exists ✔
 			file_error();
-		if ((open(argv[4], O_WRONLY | O_TRUNC | O_CREAT | O_APPEND, 0777 )) < 0) // create an outfile if it doesn't exist ✔
-			file_error();
-
-		path_cmd1 = build_path(argv[2], env);
-		path_cmd2 = build_path(argv[3], env);
-		
+		if ((open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0777 )) < 0) // create an outfile if it doesn't exist ✔
+				file_error();
+				
 		cmd1_args = ft_split(argv[2], ' ');
 		cmd2_args = ft_split(argv[3], ' ');
+		
+	
+
+		path_cmd1 = build_path(cmd1_args[0], env);
+		path_cmd2 = build_path(cmd2_args[0], env);
 		
 		if (!path_cmd1 || !path_cmd2 || !cmd1_args || !cmd2_args)
     	{
@@ -85,22 +90,21 @@ int main(int argc, char **argv, char **env)
         	file_error();
     	}
 
+		pipe(pipe_fd);
 		child1 = fork();
 		if (child1 == 0)
 		{
 			printf("Child1 has been created\n");
-        	int infile = open(argv[1], O_RDONLY);
-        	if (infile < 0)
-			{
-				printf("Error happening in Child 1\n");
-            	file_error();
-			}
+        	infile = open(argv[1], O_RDONLY);
         	dup2(infile, STDIN_FILENO); // Redirect stdin to infile
     		close(infile);
+			dup2(pipe_fd[1], STDOUT_FILENO);
+			close(pipe_fd[0]);
 
         	// Execute cmd1
         	execve(path_cmd1, cmd1_args, env);
         	perror("execve"); // If execve fails
+			
         	exit(1);
 		}
 		
@@ -108,12 +112,9 @@ int main(int argc, char **argv, char **env)
     	if (child2 == 0)
     	{
 			printf("Child2 has been created\n");
-			int outfile = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT | O_APPEND, 0777);
-			if (outfile < 0)
-			{
-				printf("Error happening in Child 2\n");
-            	file_error();
-			}
+			outfile = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0777);
+			dup2(pipe_fd[0], STDIN_FILENO);
+			close(pipe_fd[1]);
         	dup2(outfile, STDOUT_FILENO); // Redirect stdout to outfile
         	close(outfile);
 			
@@ -122,15 +123,22 @@ int main(int argc, char **argv, char **env)
         	exit(1);
     	}
 		
-		// Parent waits for both children
-    	waitpid(child1, NULL, 0);
-    	waitpid(child2, NULL, 0);
+		printf("%s\n", cmd1_args[0]);
+		printf("%s\n", cmd2_args[0]);
+		if (ft_strncmp(cmd1_args[0], "sleep", 5) == 0 || ft_strncmp(cmd2_args[0], "sleep", 5) == 0)
+		{
+			printf("hello\n");
+			waitpid(child1, NULL, 0);
+			waitpid(child2, NULL, 0);
+		}
+
 
 		// Free allocated resources
 		free(path_cmd1);
 		free(path_cmd2);
 		free_array(cmd1_args);
 		free_array(cmd2_args);
+		exit(1);
 	}
 
 
